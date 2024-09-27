4379f4a980c73bbf36b09467eb05427131f15f02
dudawl18 <mwolfluiz123a3@gmail.com>
2024-09-27 01:49:16 -0300

    Ajustes na homepage da farmacia

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,23 @@
-
+import farmarciaG from "../assets/images/farmarciaG.png"
 
 function Body() {
 
   return (
 
     <body>
-        
+      <div className="totalBody">
+        <div className="ladoEsquerdo">
+          <h2 className="titulos">Novidades</h2>
+          <img src={farmarciaG} className="imageBody" alt="duas maões em formato de concha vistas de lado viradas para cima comum coração vermelho flutuando no meio delas. Abaixo essa escrito 'Farmárcia' e 'Sob nova direção (Ex Farmárcio)" />
+        </div>
+        <div className="ladoDireito">
+          <a href="" className="topicosBody">Inauguração</a>
+          <a href="" className="topicosBody">Nova Direção</a>
+          <a href="" className="topicosBody">Campanha</a>
+        </div>
+      </div>
     </body>
-    
+
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '/src/App.css'
+import Logo from "../assets/images/farmarcia-logo-pq.png"
 
 function Header() {
 
@@ -7,13 +8,13 @@ function Header() {
 
         <header>
             <div className='header-container'>
-                <h1>Farmárcia</h1>
-                <input className='navbarInput' type="text" />
-                <a href="">Home</a>
-                <a href="">Produtos</a>
-                <a href="">Nossas ofertas</a>
-                <a href="">Ajuda</a>
-
+                <img src={Logo} className='logo' alt="duas maões em formato de concha vistas de lado viradas para cima comum coração vermelho flutuando no meio delas" />
+                <h1 className='titulos'>Farmárcia</h1>
+                <input className='navbarInput' type="text" placeholder='Pesquise seu produto' />
+                <a href="" className='topicosHeader'>Home</a>
+                <a href="" className='topicosHeader'>Produtos</a>
+                <a href="" className='topicosHeader'>Contato</a>
+                <a href="" className='topicosHeader'>Ajuda</a>
             </div>
 
         </header>
